1109475a91252c554de84b291739adecdf9178ff
kingsonwu <kingsonwuharonye@gmail.com>
2021-08-24 17:44:34 -0500

    beginning of cleaning up

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const Sequelize = require ("sequelize");
+const { Mentors } = require('./models/mentors')
 
 const server = express();
 
@@ -12,6 +14,27 @@ server.use(express.static(path.resolve(__dirname + '/react-ui/build')));
 server.get('/heartbeat', (req, res) => {
   res.send('Hello!! I am heartbeat channel!');
 });
+
+server.get("/api/mentors", async (req, res) => {
+  const mentor = await mentors.findAll();
+  res.json(mentor);
+});
+
+
+// app.post("/api/mentors", async (req, res) => {
+//   const { firstName, lastName, email, type, skills, about } = req.body;
+//   const newMentor = await Mentor.create({
+//     firstName,
+//     lastName,
+//     email,
+//     type,
+//     skills,
+//     about,
+//   });
+//   res.json({
+//     id: newMentor.id,
+//   });
+// });
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
